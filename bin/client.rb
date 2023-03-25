c288bb3725ba03f947c2eef233a39607fd602727
Diego Pablo Matias Baltar <dpmbaltar@gmail.com>
2023-03-25 12:56:03 -0300

    Usar single quotes

--- bin/client.rb
+++ bin/client.rb
@@ -6,7 +6,7 @@ require 'socket'
 
 # Opciones de línea de comandos
 options = OpenStruct.new
-options.addr = "localhost"
+options.addr = 'localhost'
 options.port = 24000
 
 # Obtener opciones
@@ -29,13 +29,13 @@ WeatherInfo = Struct.new :date, :temp, :cond do
   end
   def cond_str
     case @cond
-    when 0 then "Despejado"
-    when 1 then "Nublado"
-    when 2 then "Neblina"
-    when 3 then "Lluvia"
-    when 4 then "Chubascos"
-    when 5 then "Nieve"
-    else "Desconocida"
+    when 0 then 'Despejado'
+    when 1 then 'Nublado'
+    when 2 then 'Neblina'
+    when 3 then 'Lluvia'
+    when 4 then 'Chubascos'
+    when 5 then 'Nieve'
+    else 'Desconocida'
     end
   end
 end
@@ -43,27 +43,27 @@ end
 begin
   socket = TCPSocket.new options.addr, options.port
 
-  puts "Escribir mensaje:"
+  puts 'Escribir mensaje:'
   message = gets.chomp.strip
   socket.puts message
-  puts "Mensaje enviado."
+  puts 'Mensaje enviado.'
 
-  puts "Bytes recibidos:"
+  puts 'Bytes recibidos:'
   response = socket.recv WeatherInfo.size
   response.each_byte do |b|
-    print "%02x " % b
+    print '%02x ' % b
   end
   puts
 
   if response.bytesize >= WeatherInfo.size then
     weather_info = WeatherInfo.new.unpack response
-    puts "Datos del clima recibidos:"
-    puts "  Fecha: %s" % weather_info.date
-    puts "  Condición: %d (%s)" % [weather_info.cond, weather_info.cond_str]
-    puts "  Temperatura: %.1f" % weather_info.temp
+    puts 'Datos del clima recibidos:'
+    puts '  Fecha: %s' % weather_info.date
+    puts '  Condición: %d (%s)' % [weather_info.cond, weather_info.cond_str]
+    puts '  Temperatura: %.1f' % weather_info.temp
   end
 
   socket.close
-  puts "Conexión cerrada."
+  puts 'Conexión cerrada.'
 
-end until message.downcase == "salir"
+end until message.downcase == 'salir'
