82aa69df92095f1812e2edee59041a56731cc141
Diego Pablo Matias Baltar <dpmbaltar@gmail.com>
2023-03-26 17:11:38 -0300

    Agregar Request

--- src/util.h
+++ src/util.h
@@ -4,6 +4,8 @@
 
 /* Inicializador para Date */
 #define DATE_INIT { (uint16_t)0, (uint8_t)0, (uint8_t)0 }
+/* Inicializador para Request */
+#define REQUEST_INIT { (char*)0, (char*)0, (int)0, (int)0 }
 
 /* Tipo de datos para recepción de fechas del cliente */
 typedef struct
@@ -12,3 +14,12 @@ typedef struct
   uint8_t  month;
   uint8_t  day;
 } Date;
+
+/* Tipo de dato para manejar solicitud/respuesta entre cliente y servidor  */
+typedef struct
+{
+  void *send;
+  void *recv;
+  int   send_len;
+  int   recv_len;
+} Request;
