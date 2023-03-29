d81ce53eeb8db092cdb33aa34d0c97e5e52420a9
Diego Pablo Matias Baltar <dpmbaltar@gmail.com>
2023-03-29 00:38:37 -0300

    Agregar tipos de servidor

--- bin/client.rb
+++ bin/client.rb
@@ -8,6 +8,7 @@ require 'socket'
 options = OpenStruct.new
 options.host = 'localhost'
 options.port = 24000
+options.type = 'SP'
 
 # Obtener opciones
 OptionParser.new do |arg|
@@ -16,6 +17,9 @@ OptionParser.new do |arg|
   end
   arg.on '-p', '--port PORT', 'Puerto del servidor (i.e. -p 24000)' do |val|
     options.port = val.to_i
+  end
+  arg.on '-t', '--type TYPE', 'Tipo de servidor (SP, SC o SH)' do |val|
+    options.type = val
   end
 end.parse!
 
@@ -44,6 +48,90 @@ WeatherInfo = Struct.new :date, :temp, :cond do
   end
 end
 
+# Estructura de datos del horóscopo
+AstroInfo = Struct.new :sign, :sign_compat, :date_range, :mood do
+  attr_accessor :sign, :sign_compat, :mood
+  def self.size
+    10
+  end
+  def unpack data
+    @sign = data[0,1].unpack('C').join.to_i
+    @sign_compat = data[1,1].unpack('C').join.to_i
+    @date_range = data[2,4].unpack('C*')
+    mood_len = data[6,4].unpack('L').join.to_i
+    @mood = data[10,mood_len]
+    self
+  end
+  def range_from
+    "%02d/%02d" % [@date_range[1], @date_range[0]]
+  end
+  def range_to
+    "%02d/%02d" % [@date_range[3], @date_range[2]]
+  end
+  def sign_s
+    sign_to_s @sign
+  end
+  def sign_compat_s
+    sign_to_s @sign_compat
+  end
+  def sign_to_s sign
+    case sign
+    when 0 then 'Aries'
+    when 1 then 'Tauro'
+    when 2 then 'Gemini'
+    when 3 then 'Cancer'
+    when 4 then 'Leo'
+    when 5 then 'Virgo'
+    when 6 then 'Libra'
+    when 7 then 'Scorpio'
+    when 8 then 'Sagitario'
+    when 9 then 'Capricornio'
+    when 10 then 'Acuario'
+    when 11 then 'Piscis'
+    else 'Desconocido'
+    end
+  end
+end
+
+# Manejo de respuesta para cada tipo de servidor
+module Client
+  # Servidor principal
+  def Client.main response
+
+  end
+  # Servidor del horóscopo
+  def Client.horoscope response
+    # Leer bytes en struct AstroInfo, si hay suficientes datos
+    if response.bytesize >= AstroInfo.size then
+      info = AstroInfo.new.unpack response
+      if not info.range_from.empty? then
+        puts 'Datos del horóscopo recibidos:'
+        puts '- Signo: %d (%s)' % [info.sign, info.sign_s]
+        puts '- Compat.: %d (%s)' % [info.sign_compat, info.sign_compat_s]
+        puts '- Rango de fechas: %s - %s' % [info.range_from, info.range_to]
+        puts '- Estado: %s' % info.mood
+      else
+        puts 'Datos recibidos no válidos'
+      end
+    end
+  end
+  # Servidor del clima
+  def Client.weather response
+    # Leer bytes en struct WeatherInfo, si hay suficientes datos
+    if response.bytesize >= WeatherInfo.size then
+      weather_info = WeatherInfo.new.unpack response
+      if not weather_info.date.empty? then
+        puts 'Datos del clima recibidos:'
+        puts '- Fecha: %s' % weather_info.date
+        puts '- Condición: %d (%s)' % [weather_info.cond, weather_info.cond_str]
+        puts '- Temperatura: %.1f' % weather_info.temp
+      else
+        puts 'Datos recibidos no válidos'
+      end
+    end
+  end
+end
+
 # Ejecutar loop hasta que el usuario escriba "salir"
 loop do
   # Solicitar datos a enviar al usuario
@@ -51,7 +139,7 @@ loop do
   message = gets
   break if message.chomp.downcase == 'salir'
 
-  # Manejar fechas para el servidor del clima
+  # Manejar fechas para el servidor
   if message.match? /[0-9]{4}-[0-9]{2}-[0-9]{2}/ then
     parts = message.split('-').map { |s| s.to_i }
     message = [
@@ -77,23 +165,18 @@ loop do
 
   # Mostrar bytes recibidos
   puts 'Bytes recibidos:'
-  response = socket.recv WeatherInfo.size
+  response = socket.recv 255
   response.each_byte do |b|
     print '%02x ' % b
   end
   puts
 
-  # Leer bytes en struct WeatherInfo, si hay suficientes datos
-  if response.bytesize >= WeatherInfo.size then
-    weather_info = WeatherInfo.new.unpack response
-    if weather_info.date.empty? then
-      puts 'Datos del clima recibidos:'
-      puts '  Fecha: %s' % weather_info.date
-      puts '  Condición: %d (%s)' % [weather_info.cond, weather_info.cond_str]
-      puts '  Temperatura: %.1f' % weather_info.temp
-    else
-      puts 'Datos recibidos no válidos'
-    end
+  # Manejar respuesta según el tipo de servidor
+  case options.type.upcase
+  when 'SP' then Client::main response
+  when 'SC' then Client::weather response
+  when 'SH' then Client::horoscope response
+  else break
   end
 
   # Cerrar conexión
