21aaa837dad4fa642dd81cca139f015b78544896
Diego Pablo Matias Baltar <dpmbaltar@gmail.com>
2023-04-04 23:37:58 -0300

    Documentar funciones

--- src/tcpclient.h
+++ src/tcpclient.h
@@ -10,37 +10,62 @@
 #include <glib.h>
 #include <stdint.h>
 
-/** @brief Dominio de errores para funciones de TcpClient */
+/** Dominio de errores para funciones de TcpClient */
 #define TCP_CLIENT_ERROR (tcp_client_error_quark())
 
-/** @brief Códigos de error para funciones de TcpClient */
-typedef enum _TcpClientError
+/** Códigos de error para funciones de TcpClient */
+typedef enum
 {
   TCP_CLIENT_SOCK_ERROR,
   TCP_CLIENT_SOCK_CONNECT_ERROR,
 } TcpClientError;
 
-/** @brief Representa una configuración para un cliente TCP */
-typedef struct _TcpClient TcpClient;
+/** Contiene una configuración para un cliente TCP */
+typedef struct TcpClient TcpClient;
 
 /**
- * @brief Tipo de función para ejecutar en @link tcp_client_run()
+ * Tipo de función para ejecutar en tcp_client_run().
+ *
  * @see tcp_client_run()
  * @param sockfd conexión TCP
- * @param data puntero a datos opcionales
+ * @param data puntero a datos adicionales
  * @return el resultado de la función
  */
-typedef void *(*TcpClientFunc)          (int             sockfd,
-                                         gpointer        data);
+typedef void *(*TcpClientFunc)(int sockfd, void *data);
 
-TcpClient      *tcp_client_new          (const char     *host,
-                                         uint16_t        port);
+/**
+ * Crea una nueva configuración para un cliente TCP.
+ *
+ * @see tcp_client_free()
+ * @param host el nombre del host
+ * @param port el puerto del host
+ * @return puntero a TcpClient, NULL en caso de error (debe liberarse con
+ * tcp_client_free() cuando ya no se utilice)
+ */
+TcpClient *tcp_client_new(const char *host, uint16_t port);
 
-GThread        *tcp_client_run          (TcpClient      *client,
-                                         TcpClientFunc   func,
-                                         gpointer        func_data,
-                                         GError        **error);
+/**
+ * Abre una conexión TCP y ejecuta la función dada en un nuevo hilo.
+ *
+ * @param client el cliente TCP
+ * @param func la función a ejecutar en un nuevo hilo
+ * @param data parámetro adicional opcional para la función
+ * @param error puntero a error recuperable, debe estar inicializado a NULL
+ * @return el hilo de ejecución de la función, NULL en caso de error
+ */
+GThread *tcp_client_run(TcpClient *client, TcpClientFunc func, void *data, GError **error);
 
-void            tcp_client_free         (TcpClient      *client);
+/**
+ * Libera los recursos asignados por tcp_client_new().
+ *
+ * @see tcp_client_new()
+ * @param client puntero a TcpClient
+ */
+void tcp_client_free(TcpClient *client);
 
-GQuark          tcp_client_error_quark  (void);
+/**
+ * Devuelve el dominio de errores para el cliente TCP.
+ *
+ * @return el dominio de errores para utilizar con GError
+ */
+GQuark tcp_client_error_quark(void);
